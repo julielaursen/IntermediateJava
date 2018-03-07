510d7fd7dc860cb26c5b23a8ab4778e7a79c9b3b
Julie Laursen <Julielaursen1@gmail.com>
2018-03-06 22:48:45 -0600

    Change to Check.java

--- src/Chapter14/Check.java
+++ src/Chapter14/Check.java
@@ -7,11 +7,11 @@ public class Check {
 	public static void main(String[] args) {
 		System.out.println("Please enter an amount below $1000.00");
 		Scanner scanner = new Scanner(System.in);
-		int amount = scanner.nextInt();
+		float amount = scanner.nextFloat();
 		numToStr(amount);
 	}
 	
-	public static String numToStr(int amount) {
+	public static String numToStr(float amount) {
 
 		String[] toTwenty = { "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX",
 		        "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE",
@@ -23,22 +23,33 @@ public class Check {
 		
 		
 	    String str = "";
+	    String hundredsStr = "";
+	    String tensStr = "";
+	    String onesStr = "";
+	    float tensInt;
+	    float onesInt;
+	    
 	    if (amount >= 100) {
-	        str = toTwenty[ amount / 100] + " hundred " + numToStr(amount % 100);
-		    System.out.println(str);
-	 	    return str;
-	    }
-	    else if (amount >= 20) {
-	    	str = tens[amount / 10] + numToStr(amount % 10);
-	    	System.out.println(str);
-	    	return str;
-	    }
-	
-	    else {
-	        str += toTwenty[ amount ];
-		    System.out.println(str);
-	 	    return str;
-	    }
-	}
+	    	hundredsStr = toTwenty[ (int) (amount / 100)];
+	    	//System.out.println(hundredsStr);
+	        tensInt = amount % 100;
+	        //System.out.println("remainder is" + tensInt);
+	        onesInt = amount % 10;
+	        //System.out.println("remainder is" + onesInt);
+	    	tensStr = tens[(int) (tensInt / 10)];
+	    	//System.out.println("tensStr is " + tensStr);
+	    	onesStr = toTwenty[(int) (onesInt % 10)];
+	        str = hundredsStr + " hundred " +  tensStr + onesStr;
+ 	    }
+	    else if (amount < 100 && amount >= 20) {
+	    	str = tens[(int) (amount / 10)] + toTwenty[(int) (amount % 10)];
+  	    }
+	    else if(amount < 20){
+	        str = toTwenty[ (int) amount ];
+  	    }
+	    System.out.println(str);
+	    return str;
 
+
+	}
 }
