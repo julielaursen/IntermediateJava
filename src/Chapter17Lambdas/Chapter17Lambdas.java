a40ac67de7594a4c19abc148ee6f85616016ac05
julielaursen <Julielaursen1@gmail.com>
2018-04-05 21:37:30 -0500

    commit invoice tables

--- src/Chapter17Lambdas/Chapter17Lambdas.java
+++ src/Chapter17Lambdas/Chapter17Lambdas.java
@@ -1,6 +1,7 @@
 package Chapter17Lambdas;
 
 
+
 import java.util.*;
 import java.util.Comparator;
 import java.util.List;
@@ -12,61 +13,78 @@ import java.util.stream.Collectors;
 
 public class Chapter17Lambdas {
 
-	public static void main(String[] args)
-	{
-		//initials table of parts
-		Invoice[] invoices = {
-				new Invoice(83, "Electric sander", 7, 57.98),
-				new Invoice(24, "Power saw", 18, 99.99),
-				new Invoice(7, "Sledge hammer", 11, 21.50),
-				new Invoice(77, "Hammer", 76, 11.99),
-				new Invoice(39, "Lawn Mower", 3, 79.50),
-				new Invoice(68, "Screwdriver", 106, 6.99),
-				new Invoice(56, "Jig Saw", 21, 11.00),
-				new Invoice(3, "Wrench", 34, 7.50)
-		};
-		
-		Predicate<Invoice> twoToFourHundred = e -> (e.getPrice() >= 12 && e.getPrice() <= 400);
-		
-		List<Invoice> list = Arrays.asList(invoices);
-		System.out.println("Create invoice list:");
-		//list.stream().forEach(System.out::println);
-		
-		//print values in sorted order by part description
-		System.out.printf("%nValues displayed in sorted order by description:%n");
-		list.stream()
-			.sorted(Comparator.comparing(Invoice::getPartDescription))
-			.forEach(System.out::println);
-		
-		//print values in sorted order by price
-		System.out.printf("%nValues displayed in sorted order by price:%n");
-		list.stream()
-			.sorted(Comparator.comparing(Invoice::getPrice))
-			.forEach(System.out::println);
-		
-		//map each invoice to its part description and value of invoice (quantity times price)
-		System.out.printf("%nInvoices mapped to description and quantity:%n");
-		//page 755
-		Map<String, List<Invoice>> groupByPartValue =
-				list.stream()
-					.collect(Collectors.groupingBy(Invoice::getPartDescription));
-					groupByPartValue.forEach(
-					(partDescription, quantity) ->
-					{
-					System.out.println(partDescription);
-					quantity.forEach( invoice -> System.out.printf(partDescription, quantity));
-					}
-				);
- 		//use lambdas and streams to map each invoice to its part description and quantity, sort by quantity, and display the price
-		
-		
-		
-		//print values from $200-400
-		System.out.printf("%nValues displayed in the range of $200-$400",
-		list.stream()
-			.filter(twoToFourHundred)
-			.findFirst()
-			.get());
-		
-	}
+ public static void main(String[] args)
+ {
+  //initials table of parts
+  Invoice[] invoices = {
+    new Invoice(83, "Electric sander", 7, 57.98),
+    new Invoice(24, "Power saw", 18, 99.99),
+    new Invoice(7, "Sledge hammer", 11, 21.50),
+    new Invoice(77, "Hammer", 76, 11.99),
+    new Invoice(39, "Lawn Mower", 3, 79.50),
+    new Invoice(68, "Screwdriver", 106, 6.99),
+    new Invoice(56, "Jig Saw", 21, 11.00),
+    new Invoice(3, "Wrench", 34, 7.50)
+  };
+  
+  Predicate<Invoice> twoToFourHundred = e -> (e.getTotalPrice() >= 200 && e.getTotalPrice() <= 400);
+  
+  List<Invoice> list = Arrays.asList(invoices);
+  System.out.println("Create invoice list:");
+  //list.stream().forEach(System.out::println);
+  
+  //a) print values in sorted order by part description
+  System.out.printf("%nValues displayed in sorted order by description:%n");
+  list.stream()
+   .sorted(Comparator.comparing(Invoice::getPartDescription))
+   .forEach(System.out::println);
+  
+  //b) print values in sorted order by price
+  System.out.printf("%nValues displayed in sorted order by price:%n");
+  list.stream()
+   .sorted(Comparator.comparing(Invoice::getPrice))
+   .forEach(System.out::println);
+  
+  //map each invoice to its part description and quantity, sort result by Quantity
+  System.out.printf("%nInvoices mapped to description and quantity, sorted by quantity:%n");
+  Map<String, Integer> groupByPartQuantity = 
+		    list.stream()
+	    	.sorted(Comparator.comparing(Invoice::getQuantity))
+		    .collect(Collectors.toMap(Invoice::getPartDescription, Invoice::getQuantity));
+  			groupByPartQuantity.forEach(
+  					 (partDescription, quantity) ->
+  				     {
+  					     System.out.println(String.format("%20s", partDescription + " " + quantity));
+  				     }
+		    );
+		  
+    
+  
+  //d map each invoice to its part description and the value of the invoice (quantity times price)
+  System.out.printf("%nInvoices mapped to description and total price (quantity times price):%n");
+  //page 755
+  Map<String, Float> groupByPartValue = 
+    list.stream()
+    .collect(Collectors.toMap(Invoice::getPartDescription, Invoice::getTotalPrice));
+    //.map(a, b) -> a * b)
+    //.collect(Invoice::getQuantity,Invoice::getPrice);
+     //.collect(Collectors.groupingBy(Invoice::getPartDescription, Invoice::getTotalPrice));// Collectors.summingInt(Invoice::getQuantity)));
+  //Collectors.reduce((a, b) -> a * b).collect(Invoice::getQuantity,Invoice::getPrice) );//
+     groupByPartValue.forEach(
+     (partDescription, quantity) ->
+     {
+    	 System.out.println(String.format("%20s", partDescription + "\t" + quantity));
+     }
+    );
+  
+ 
+  //print values from $200-400
+  System.out.printf("%nValues displayed in the range of $200-$400%n");
+   list.stream()
+   .filter(twoToFourHundred)
+   .sorted(Comparator.comparing(Invoice::getTotalPrice))
+   .forEach(System.out::println);
+//  
+ }
+
 }
--- src/Chapter17Lambdas/Invoice.java
+++ src/Chapter17Lambdas/Invoice.java
@@ -1,77 +1,80 @@
 package Chapter17Lambdas;
-
 public class Invoice {
-	
-	private int partNumber;
-	private String partDescription;
-	private int quantity;
-	private double price;
-	
-	public Invoice(int partNumber, String partDescription, int quantity, double price)
-	{
-		this.partNumber = partNumber;
-		this.partDescription = partDescription;
-		this.quantity = quantity;
-		this.price = price;
-	}
-	
-	//set partNumber;
-	public void setPartNumber(int partNumber)
-	{
-		this.partNumber = partNumber;
-	}
-	
-	//get partNumber
-	public int getPartNumber()
-	{
-		return partNumber;
-	}
-	
-	//set partDescription
-	public void setPartDescription(String partDescription)
-	{
-		this.partDescription = partDescription;
-	}
-	
-	//get partDescription
-	public String getPartDescription()
-	{
-		return partDescription;
-	}
-	
-	//set quantity
-	public void setQuantity(int quantity)
-	{
-		this.quantity = quantity;
-	}
-	
-	//get quantity
-	public int getQuantity()
-	{
-		return quantity;
-	}
-	
-	//set price
-	public void setPrice(double price)
-	{
-		this.price = price;
-	}
-	
-	//get price
-	public double getPrice()
-	{
-		return price;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return String.format("%-10d%20s%10d%10.2f", getPartNumber(), getPartDescription(), getQuantity(), getPrice());
-	}
+	 
+	 private int partNumber;
+	 private String partDescription;
+	 private int quantity;
+	 private double price;
+	 
+	 public Invoice(int partNumber, String partDescription, int quantity, double price)
+	 {
+	  this.partNumber = partNumber;
+	  this.partDescription = partDescription;
+	  this.quantity = quantity;
+	  this.price = price;
+	 }
+	 
+	 public float getTotalPrice()
+	 {
+	   return (float) (this.quantity * this.price);
+	 }
+	 
+	 //set partNumber;
+	 public void setPartNumber(int partNumber)
+	 {
+	  this.partNumber = partNumber;
+	 }
+	 
+	 //get partNumber
+	 public int getPartNumber()
+	 {
+	  return partNumber;
+	 }
+	 
+	 //set partDescription
+	 public void setPartDescription(String partDescription)
+	 {
+	  this.partDescription = partDescription;
+	 }
+	 
+	 //get partDescription
+	 public String getPartDescription()
+	 {
+	  return partDescription;
+	 }
+	 
+	 //set quantity
+	 public void setQuantity(int quantity)
+	 {
+	  this.quantity = quantity;
+	 }
+	 
+	 //get quantity
+	 public int getQuantity()
+	 {
+	  return quantity;
+	 }
+	 
+	 //set price
+	 public void setPrice(double price)
+	 {
+	  this.price = price;
+	 }
+	 
+	 //get price
+	 public double getPrice()
+	 {
+	  return price;
+	 }
+	 
+	 @Override
+	 public String toString()
+	 {
+	  return String.format("%-10d%20s%10d%10.2f", getPartNumber(), getPartDescription(), getQuantity(), getPrice());
+	 }
 
-	public int compareToIgnoreCase(String string) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+	 public int compareToIgnoreCase(String string) {
+	  // TODO Auto-generated method stub
+	  return 0;
+	 }
 }
